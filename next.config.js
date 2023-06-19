5d10a8f7ebb9dbd4555dab57337433d67dba89c5
Huzaifa Mustafa <hello@huzaifamustafa.com>
2023-06-19 17:58:01 +0500

    adjust deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 const { withContentlayer } = require('next-contentlayer')
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',
+  distDir: '_static',
+  images: {
+    unoptimized: true
+  }
+}
 
 module.exports = withContentlayer(nextConfig)
